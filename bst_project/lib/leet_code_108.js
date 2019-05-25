c7a8fa0388a6edb172868ae5dd5f69103fda7a39
jinfull <jinfull@gmail.com>
2019-05-25 16:39:34 -0700

    leet code problems completed

--- bst_project/lib/leet_code_108.js
+++ bst_project/lib/leet_code_108.js
@@ -1,7 +1,13 @@
 // View the full problem and run the test cases at:
 //  https://leetcode.com/problems/convert-sorted-array-to-binary-search-tree/
 
+var sortedArrayToBST = function (nums) {
+  if (!nums.length) return null;
 
-function sortedArrayToBST(nums) {
+  let midIdx = Math.floor(nums.length / 2);
+  let root = new TreeNode(nums[midIdx]);
+  root.left = sortedArrayToBST(nums.slice(0, midIdx));
+  root.right = sortedArrayToBST(nums.slice(midIdx + 1));
 
+  return root;
 }
