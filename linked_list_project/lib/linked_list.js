ccd5833167ef1f504ca08ce5346ee919d25f029f
jinfull <jinfull@gmail.com>
2019-05-26 13:55:45 -0700

    Finish linked list methods

--- linked_list_project/lib/linked_list.js
+++ linked_list_project/lib/linked_list.js
@@ -78,42 +78,104 @@ class LinkedList {
 
     // TODO: Implement the addToHead method here
     addToHead(val) {
+        let newNode = new Node(val);
 
+        if (!this.head) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            newNode.next = this.head;
+            this.head = newNode;
+        }
+
+        this.length++;
+        return this;
     }
 
     // TODO: Implement the removeHead method here
     removeHead() {
+        if (!this.head) return undefined;
+        const currentHead = this.head;
+        this.head = currentHead.next;
+        this.length--;
 
+        if (this.length === 0) {
+            this.tail = null;
+        }
+
+        return currentHead;
     }
 
     // TODO: Implement the contains method here
     contains(target) {
+        let node = this.head;
 
+        while (node) {
+            if (node.value === target) return true;
+            node = node.next;
+        }
+
+        return false;
     }
 
     // TODO: Implement the get method here
     get(index) {
+        if (index < 0 || index >= this.length) return null;
 
+        let counter = 0;
+        let current = this.head;
+
+        while (counter !== index) {
+            current = current.next;
+            counter++;
+        }
+
+        return current;
     }
 
     // TODO: Implement the set method here
     set(index, val) {
+        const foundNode = this.get(index);
 
+        if (foundNode) {
+            foundNode.value = val;
+            return true;
+        }
+
+        return false;
     }
 
     // TODO: Implement the insert method here
     insert(index, val) {
+        if (index < 0 || index >= this.length) return false;
+        if (index === this.length) return !!this.addToTail(val);
+        if (index === 0) return !!this.addToHead(val);
 
+        const newNode = new Node(val);
+        const prev = this.get(index - 1);
+        const temp = prev.next;
+        prev.next = newNode;
+        newNode.next = temp;
+        this.length++;
+        return true;
     }
 
     // TODO: Implement the remove method here
     remove(index) {
+        if (index < 0 || index >= this.length) return undefined;
+        if (index === 0) return this.removeHead();
+        if (index === this.length - 1) return this.removeTail();
 
+        const prevNode = this.get(index - 1);
+        const removed = prevNode.next;
+        prevNode.next = removed.next;
+        this.length--;
+        return removed;
     }
 
     // TODO: Implement the size method here
     size() {
-
+        return this.length;
     }
 }
 
